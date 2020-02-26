7e132d6d4ce5536db1476eaede54cedb009abfaa
Jacco <jaccodouma@gmail.com>
2020-02-26 16:57:50 +0100

    Put all models in array

--- main.js
+++ main.js
@@ -16,218 +16,122 @@ document.body.appendChild(renderer.domElement);
 
 var objLoader = new THREE.ObjectLoader();
 
-objLoader.load(
-	"models/room.json",
-	function ( obj ) {
-		scene.add( obj );
+var models = [
+	{
+		file: "room",
+		position: [0,0,0],
+		rotation: [0,0,0]
+	},
+	{
+		file: "cupboard",
+		position: [0.398793,1.69392,0],
+		rotation: [0,0,180]
+	},
+	{
+		file: "tv",
+		position: [0.396517,1.618,1],
+		rotation: [0,0,180]
+	},
+	{
+		file: "desk",
+		position: [-0.624001,1.5851,0],
+		rotation: [0,0,0]
+	},
+	{
+		file: "shelves",
+		position: [-1.63318,1.48885,0],
+		rotation: [0,0,90]
+	},
+	{
+		file: "chimney",
+		position: [-1.77504,0.492233,0],
+		rotation: [0,0,90]
+	},
+	{
+		file: "bedframe",
+		position: [-0.853212,-0.890585,0],
+		rotation: [0,0,0]
+	},
+	{
+		file: "mattress",
+		position: [-0.853212,-0.890585,0.464277],
+		rotation: [0,0,0]
+	},
+	{
+		file: "table",
+		position: [0.30847,-0.86573,0],
+		rotation: [0,0,9.333]
+	},
+	{
+		file: "bin",
+		position: [2.07835,0.113945,0],
+		rotation: [0,0,204]
+	},
+	{
+		file: "clock",
+		position: [1.7029,-1.95009,1.69322],
+		rotation: [90,0,0]
+	},
+	{
+		file: "kitchen_cabinet",
+		position: [2.8,-1.45,0],
+		rotation: [0,0,270]
+	},
+	{
+		file: "fridge",
+		position: [2.79242,-0.67923,0],
+		rotation: [0,0,270]
+	},
+	{
+		file: "cabinets",
+		position: [2.88982,-1.15619,1.53193],
+		rotation: [0,0,270]
+	},
+	{
+		file: "couch",
+		position: [1.37328,-1.18505,0],
+		rotation: [0,0,270]
+	},
+	{
+		file: "door",
+		position: [0,0,0],
+		rotation: [0,0,0]
+	},
+	{
+		file: "windows",
+		position: [0,0,0],
+		rotation: [0,0,0]
+	},
+	{
+		file: "shelves_2",
+		position: [-1.79182,-0.317944,0],
+		rotation: [0,0,90]
+	},
+	{
+		file: "radiator",
+		position: [1.81458,0.765988,0],
+		rotation: [0,0,270]
 	}
-);
+]
 
-objLoader.load(
-	"models/cupboard.json",
-	function ( obj ) {
-	  scene.add( obj );
+// Load in models
+models.forEach(element => {
+	objLoader.load(
+		"models/" + element.file + ".json",
+		function ( obj ) {
+			scene.add( obj );
 
-	  obj.position.x = 0.398793; 
-	  obj.position.z = 1.69392; 
-	  
-	  obj.rotation.y = THREE.Math.degToRad(180);
-	}
-);
+			obj.position.x = element.position[0];
+			obj.position.z = element.position[1];
+			obj.position.y = element.position[2];
+			obj.rotation.x = THREE.Math.degToRad(element.rotation[0]);
+			obj.rotation.z = THREE.Math.degToRad(element.rotation[1]);
+			obj.rotation.y = THREE.Math.degToRad(element.rotation[2]);
+		}
+	);
+});
 
-objLoader.load(
-	"models/tv.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 0.396517; 
-	  obj.position.z = 1.618; 
-	  obj.position.y = 1;
-	  
-	  obj.rotation.y = THREE.Math.degToRad(180);
-	}
-);
-
-objLoader.load(
-	"models/desk.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = -0.624001; 
-	  obj.position.z = 1.5851; 
-	}
-);
-
-objLoader.load(
-	"models/shelves.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = -1.63318; 
-	  obj.position.z = 1.48885; 
-	  
-	  obj.rotation.y = THREE.Math.degToRad(90);
-	}
-);
-
-objLoader.load(
-	"models/chimney.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = -1.77504; 
-	  obj.position.z = 0.492233; 
-	  
-	  obj.rotation.y = THREE.Math.degToRad(90);
-	}
-);
-
-objLoader.load(
-	"models/bedframe.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = -0.853212; 
-	  obj.position.z = -0.890586; 
-	}
-);
-
-objLoader.load(
-	"models/mattress.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = -0.853212; 
-	  obj.position.z = -0.890585; 
-	  obj.position.y = 0.464277;
-	}
-);
-
-objLoader.load(
-	"models/table.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 0.30847; 
-	  obj.position.z = -0.86573; 
-	  
-	  obj.rotation.y = THREE.Math.degToRad(9.333);
-	}
-);
-
-objLoader.load(
-	"models/bin.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 2.07835; 
-	  obj.position.z = 0.113945;
-
-	  obj.rotation.y = THREE.Math.degToRad(204);
-	}
-);
-
-objLoader.load(
-	"models/clock.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 1.7029; 
-	  obj.position.z = -1.95009;
-	  obj.position.y = 1.69322;
-
-	  obj.rotation.x = THREE.Math.degToRad(90);
-	}
-);
-
-objLoader.load(
-	"models/kitchen_cabinet.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 2.8; 
-	  obj.position.z = -1.45;
-
-	  obj.rotation.y = THREE.Math.degToRad(270);
-	}
-);
-
-objLoader.load(
-	"models/fridge.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 2.79242; 
-	  obj.position.z = -0.67923;
-
-	  obj.rotation.y = THREE.Math.degToRad(270);
-	}
-);
-
-objLoader.load(
-	"models/cabinets.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 2.88982; 
-	  obj.position.z = -1.15619;
-	  obj.position.y = 1.53193;
-
-	  obj.rotation.y = THREE.Math.degToRad(270);
-	}
-);
-
-objLoader.load(
-	"models/couch.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 1.37328; 
-	  obj.position.z = -1.18505;
-
-	  obj.rotation.y = THREE.Math.degToRad(270);
-	}
-);
-
-objLoader.load(
-	"models/door.json",
-	function ( obj ) {
-	  scene.add( obj );
-	}
-);
-
-objLoader.load(
-	"models/windows.json",
-	function ( obj ) {
-	  scene.add( obj );
-	}
-);
-
-objLoader.load(
-	"models/shelves_2.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = -1.79182; 
-	  obj.position.z = -0.317944; 
-	  
-	  obj.rotation.y = THREE.Math.degToRad(90);
-	}
-);
-
-objLoader.load(
-	"models/radiator.json",
-	function ( obj ) {
-	  scene.add( obj );
-
-	  obj.position.x = 1.81458; 
-	  obj.position.z = 0.765988; 
-	  
-	  obj.rotation.y = THREE.Math.degToRad(270);
-	}
-);
-
-// var ambient = new THREE.AmbientLight( 0x404040 );
+// var ambient = new THREE.AmbientLight( 0x403030 );
 // scene.add( ambient );
 
 // directional - KEY LIGHT
@@ -235,16 +139,16 @@ keyLight = new THREE.DirectionalLight( 0xdddddd, .7 );
 keyLight.position.set( -80, 60, 80 );
 scene.add( keyLight );
 
-//keyLightHelper = new THREE.DirectionalLightHelper( keyLight, 15 );
-//scene.add( keyLightHelper );
+keyLightHelper = new THREE.DirectionalLightHelper( keyLight, 15 );
+scene.add( keyLightHelper );
 
 // directional - FILL LIGHT
 fillLight = new THREE.DirectionalLight( 0xdddddd, .3 );
 fillLight.position.set( 80, 40, 40 );
 scene.add( fillLight );
 
-//fillLightHelper = new THREE.DirectionalLightHelper( fillLight, 15 );
-//scene.add( fillLightHelper );
+fillLightHelper = new THREE.DirectionalLightHelper( fillLight, 15 );
+scene.add( fillLightHelper );
 
 // directional - RIM LIGHT
 rimLight = new THREE.DirectionalLight( 0xdddddd, .6 );
